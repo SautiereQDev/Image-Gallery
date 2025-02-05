1108daf8adf8fd0db543847d29096c6625fb32e6
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2025-02-05 19:50:51 +0100

    add aria label

--- src/components/MainPicture.tsx
+++ src/components/MainPicture.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {FaChevronLeft, FaChevronRight} from 'react-icons/fa';
-import {Thumbnail} from './Thumbnail';
-import {IMainPictureProps} from '../interfaces/mainPicture';
-import {useGallery} from '../hooks/useGallery';
-import {ButtonLeft, ButtonRight, Container} from '../styles/mainPicture.styles';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Thumbnail } from './Thumbnail';
+import { IMainPictureProps } from '../interfaces/mainPicture';
+import { useGallery } from '../hooks/useGallery';
+import { ButtonLeft, ButtonRight, Container } from '../styles/mainPicture.styles';
 
 export const MainPicture = ({
 															buttonColor = '#FFF',
@@ -13,24 +13,24 @@ export const MainPicture = ({
 															unit,
 															...args
 														}: IMainPictureProps) => {
-	const {activeImage, storedImages, dispatch} = useGallery();
+	const { activeImage, storedImages, dispatch } = useGallery();
 
 	const setPreviusPicture = () => {
-		dispatch({type: 'PREVIOUS_ACTIVE_IMAGE'});
+		dispatch({ type: 'PREVIOUS_ACTIVE_IMAGE' });
 	};
 	const setNextPicture = () => {
-		dispatch({type: 'NEXT_ACTIVE_IMAGE'});
+		dispatch({ type: 'NEXT_ACTIVE_IMAGE' });
 	};
 
 	return (
 		<Container width={width ?? 100} height={height ?? 100} unit={unit ?? 'viewport_ratio'}>
-			<ButtonLeft onClick={setPreviusPicture}>
-				<FaChevronLeft size={buttonSize} color={buttonColor}/>
+			<ButtonLeft onClick={setPreviusPicture} aria-label={'Previous image'}>
+				<FaChevronLeft size={buttonSize} color={buttonColor} />
 			</ButtonLeft>
 			<Thumbnail src={storedImages[activeImage].src} alt={storedImages[activeImage].alt} {...args} width={100}
-								 height={100} unit={'%'}/>
-			<ButtonRight onClick={setNextPicture}>
-				<FaChevronRight size={buttonSize} color={buttonColor}/>
+								 height={100} unit={'%'} />
+			<ButtonRight onClick={setNextPicture} aria-label={'Next image'}>
+				<FaChevronRight size={buttonSize} color={buttonColor} />
 			</ButtonRight>
 		</Container>
 	);
